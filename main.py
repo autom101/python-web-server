7b794738e9b882b7e06b053735abac12ae0e6072
autom101 <122489814+autom101@users.noreply.github.com>
2024-12-19 13:44:43 -0700

    Add server initialization on port 3000

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
     # Handle GET requests
     def do_GET(self):
         self.send_response(200)
+
+
+if __name__ == '__main__':
+    serverAddress = ('', 3000)
+    server = HTTPServer(serverAddress, HTTPRequestHandler)
+    server.serve_forever()
