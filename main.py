d5ca2111dc8afefca8175eda85db0fb15613c126
autom101 <122489814+autom101@users.noreply.github.com>
2024-12-19 13:30:29 -0700

    Update import for Python 3 compatibility - Replace BaseHTTPServer with http.server

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import BaseHTTPServer
+from http.server import BaseHTTPRequestHandler
 
-class HTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-  # Handle GET requests
-  def do_GET(self):
-    self.send_response(200)
+
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+    # Handle GET requests
+    def do_GET(self):
+        self.send_response(200)
